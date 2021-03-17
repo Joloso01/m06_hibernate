ba68177b83499aff1f7ea29cbb70d5037e3ca173
jose <jbermejop01@elpuig.xeill.net>
2021-03-17 18:20:20 +0100

    Initial commit

---
+++ src/main/java/MainMagacine.java
@@ -0,0 +1,71 @@
+import Controller.FileAccessor;
+import model.Article;
+import model.Revista;
+import view.Menu;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainMagacine {
+    public static void main(String[] args)  {
+        ArrayList<Revista> revistes = new ArrayList();
+        FileAccessor fa;
+
+        Menu menu = new Menu();
+        int opcio;
+
+        opcio = menu.menuPral();
+
+        switch (opcio) {
+
+            case 1:
+
+                System.out.println("1!!");
+                fa=new FileAccessor();
+                try {
+                    fa.readAutorsFile("src/main/java/autors.txt");
+                 //   fa.printAutors();
+                   // fa.readMagazinesFile("src/main/java/revistes.txt");
+                    //fa.printRevistes();
+                    //revistes=fa.readArticlesFile("src/main/java/articles.txt");
+                    //mostraRevistes(revistes);
+                } catch (NumberFormatException | IOException e) {
+
+                    e.printStackTrace();
+                }
+                break;
+
+            default:
+                System.out.println("Adeu!!");
+                System.exit(1);
+                break;
+
+        }
+
+    }
+    public static void mostraRevistes(ArrayList<Revista> revistes){
+        for (int i = 0; i < revistes.size(); i++) {
+
+            System.out.println(revistes.get(i).toString());
+            for (int j = 0; j < revistes.get(i).getArticles().size(); j++) {
+                System.out.println("\t"+ revistes.get(i).getArticle(j).toString());
+                System.out.println("\t\t"+revistes.get(i).getArticle(j).getAutor().toString());
+            }
+
+        }
+    }
+
+    public static Revista seleccionaRevista(ArrayList<Revista> revistes){
+        //TODO
+
+        return null;
+
+    }
+    public static Article seleccionaArticle(ArrayList<Revista> revista){
+        //TODO
+
+        return null;
+
+    }
+}
